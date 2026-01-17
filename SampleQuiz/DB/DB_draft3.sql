f697c4234613e1eb8f0d4016c894cb8031660648
BCS21090011 <100390618+BCS21090011@users.noreply.github.com>
2026-01-17 19:49:09 +0800

    Redesigning database

---
+++ SampleQuiz/DB/DB_draft3.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS SampleQuiz_RoadSafety;
+
+CREATE DATABASE SampleQuiz_RoadSafety;
+
+USE SampleQuiz_RoadSafety;
+
+CREATE TABLE Roles (
+    Rolename VARCHAR(16) NOT NULL,
+    RoleLevel INT NOT NULL,
+    PRIMARY KEY (Rolename)
+);
+
+INSERT INTO Roles
+    (Rolename, RoleLevel)
+VALUES
+    ("ADMIN", 99),
+    ("PLAYER", 10);
+
+CREATE TABLE User (
+    ID INT NOT NULL AUTO_INCREMENT,
+    UserName VARCHAR(255) NOT NULL UNIQUE,
+    UserPassword VARCHAR(255) NOT NULL,
+    UserRole VARCHAR(16) NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (UserRole) REFERENCES Roles(Rolename)
+);
+
+CREATE TABLE Scores (
+    ID INT NOT NULL AUTO_INCREMENT,
+    UserID INT NOT NULL,
+    LevelID INT NOT NULL,
+    QuizMark INT NOT NULL DEFAULT 0,
+    TotalQuizMark INT NOT NULL,
+    StartDatetime BIGINT NOT NULL,
+    CompletionDatetime BIGINT DEFAULT NULL,
+    QuizInfo JSON NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (UserID) REFERENCES User(ID) ON DELETE CASCADE 
+);
